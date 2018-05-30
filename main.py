5e4a15e157531c58c2f0bc9de3fdb27a27045567
Riley <riley_kilgore@outlook.com>
2018-05-30 13:41:58 -0700

    Finished building and training a model on the iris dataset.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pandas as pd
-import sklearn as sk
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+'''The following is the training of a logistic regression model upon the iris dataset. Only 70% of the data
+is used and there are no predictions made within this file.'''
+
 
 data = pd.read_csv("iris.data", names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Type"])
-print(data)
+
+data = data.sample(frac=1).reset_index(drop=True)
+
+train, test = train_test_split(data, train_size=.7, test_size=.3)
+
+model = LogisticRegression()
+
+train_X, train_y = np.split(train,[-1],axis=1)
+
+model.fit(train_X, train_y)
+
+test_X, test_y = np.split(test, [-1], axis=1)
